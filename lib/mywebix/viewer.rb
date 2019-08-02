b6cea60958269a47c5d28add65e097faff436461
Rich Davis <vgcrld@gmail.com>
2019-08-02 16:31:36 -0400

    added webix libs

--- lib/mywebix/viewer.rb
+++ lib/mywebix/viewer.rb
@@ -16,29 +16,12 @@ module MyWebix
       enable :corss_origin
     end
     
-    get '/settings' do
-      haml :settings
-    end
-
     before do
       response.headers['Access-Control-Allow-Origin'] = '*'
     end
 
     get '/' do
-      haml :doc
-    end
-
-    get '/control/:instance/:command' do
-      instance=params[:instance].to_sym
-      command=params[:command].to_sym
-      $server[instance].send(command)
-    end
-
-    get '/query/:instance/:query' do
-      instance=params[:instance].to_sym
-      query=params[:query]
-      data = $server[instance.to_sym].exec(query)
-      data.to_json
+      haml :mywebix
     end
 
   end
